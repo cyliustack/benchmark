cf05e05ab6b6648bc1593d4cbe1552ef4a9d2648
cyliu <cyliu@localhost.localdomain>
2019-12-17 17:57:46 +0800

    update dataset

--- dataset/rename_and_label.py
+++ dataset/rename_and_label.py
@@ -1,15 +1,20 @@
 import os
 import pandas as pd
+import random
 
 df = pd.DataFrame(columns=['image', 'label'])
+classes = ['Jeng-Wu','Chu-Wu']
+df_classes = pd.DataFrame({'class':classes})
 i = 0
 for filename in os.listdir("."):
     if filename.find('.jpg') != -1:
         new_filename = 'image-%03d.jpg' % (i+1)
         os.rename(filename, new_filename  )
-        df.loc[i] = [ new_filename, 0 ] 
+        label = classes[random.randint(0,len(df_classes)-1)]
+        df.loc[i] = [ new_filename, label ] 
         i = i + 1
 
 
-df.to_csv('labels.txt', sep=' ', header=True)
+df.to_csv('labels.txt', sep=' ', header=True, index=False)
+df_classes.to_csv('classes.txt', sep=' ', header=True, index=False)
 
